4efc1f74d7f5996b3ed3bf09d19f6d227d3ee816
mika <strarti@bk.ru>
2025-03-07 16:46:13 +0600

    finished

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -8,10 +8,62 @@ const pages = [
 const links = document.querySelectorAll('.nav-link');
 
 let currPageIndex = 0;
+let currInfoIndex = 0;
+let screenStatus= ''
+loadPage(currPageIndex)
+
+
+function FillInfo(index) {
+    fetch('data.json')
+      .then(response => response.json())
+      .then(data => {
+        let localData = null
+        currInfoIndex = index
+        switch (currPageIndex) {
+            case 1:
+                localData = data.destinations[index]
+                document.querySelector('.planet_img').src = localData.images.webp
+                document.querySelector('.planet_name').textContent = localData.name
+                document.querySelector('.planet_paragraph').textContent = localData.description
+                document.getElementById('distance').innerHTML = `AVG. DISTANCE <br> <span class="info">${localData.distance}</span>`
+                document.getElementById('travel_time').innerHTML = `EST. TRAVEL TIME  <br> <span class="info">${localData.travel}</span>`
+                break;
+            case 2:
+                localData = data.crew[index]
+                document.querySelector('.speciality').textContent = localData.role
+                document.querySelector('.member_name').textContent = localData.name
+                document.querySelector('.member_bio').textContent = localData.bio
+                document.querySelector('.crew_member-img').src = localData.images.webp
+                break;
+            case 3:
+                localData = data.technology[index]
+                document.querySelector('.tech-img').src = screenStatus === 'tablet' ? localData.images.landscape : localData.images.portrait
+                document.querySelector('.tech_name').textContent = localData.name
+                document.querySelector('.tech_description').textContent = localData.description
+                break;
+            default:
+                break;
+        }
+      })
+      .catch(error => {
+        console.error('Ошибка при загрузке JSON:', error);
+      });
+}
+function ChangeInfo() {
+    const changeBtns = document.querySelectorAll('.change-btn')
+    changeBtns.forEach((btn, index) =>{
+        btn.addEventListener('click', () =>{
+            btn.classList.toggle('active')
+            changeBtns[currInfoIndex].classList.remove('active')
+            FillInfo(index)
+        })
+    })
+}
 
 function ChangeBackgrounds() {
+
     const body = document.body;
-    const pageClass = pages[parseInt(currPageIndex)]
+    const pageClass = pages[currPageIndex]
     const screenWidth = window.innerWidth;
 
     let imageUrl = '';
@@ -20,6 +72,7 @@ function ChangeBackgrounds() {
         imageUrl = `./assets/${pageClass}/background-${pageClass}-mobile.jpg`;
       } else if (screenWidth < 1440) {
         imageUrl = `./assets/${pageClass}/background-${pageClass}-tablet.jpg`;
+        screenStatus = 'tablet'
       } else {
         imageUrl = `./assets/${pageClass}/background-${pageClass}-desktop.jpg`;
     }
@@ -46,11 +99,10 @@ async function loadPage(index) {
         if (!response.ok) throw new Error('Страница не найдена');
         const html = await response.text();
         content.innerHTML = html;
-        currPageIndex = index; // Обновляем текущий индекс
+        currPageIndex = parseInt(index); // Обновляем текущий индекс
+        if (currPageIndex > 0) ChangeInfo()
         ChangeBackgrounds()
     } catch (error) {
         content.innerHTML = '<h1>Ошибка загрузки</h1>';
     }
 }
-
-loadPage(0)
